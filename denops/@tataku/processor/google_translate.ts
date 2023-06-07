c295ebf7c3e45bfa14eea5ead0766919fa0272a1
Omochice <mochice.mls.ntl@gmail.com>
2023-06-07 21:28:57 +0900

    :muscle: use stream api instead

--- denops/@tataku/processor/google_translate.ts
+++ denops/@tataku/processor/google_translate.ts
@@ -1,38 +1,32 @@
 import { Denops } from "https://deno.land/x/denops_std@v5.0.0/mod.ts";
 import { GTR } from "https://deno.land/x/gtr@v0.0.1/mod.ts";
-import {
-  isObject,
-  isString,
-} from "https://deno.land/x/unknownutil@v2.1.1/mod.ts";
-import { Processor } from "https://raw.githubusercontent.com/Omochice/tataku.vim/master/denops/tataku/interface.ts";
+import { $object, $string, access } from "https://esm.sh/lizod@0.2.4";
 
-export default class implements Processor {
-  private readonly gtr = new GTR();
-  constructor(private readonly option: Record<string, unknown>) {
-    if (!isOption(option)) {
-      throw new Error(
-        `option must have "source" and "target" field: ${
-          JSON.stringify(option)
-        }`,
-      );
-    }
+const isOption = $object({
+  source: $string,
+  target: $string,
+});
+
+const processor = (_: Denops, option: unknown) => {
+  const ctx = { errors: [] };
+  if (!isOption(option, ctx)) {
+    throw new Error(
+      ctx.errors
+        .map((e) => `error at ${e} ${access(option, e)}`)
+        .join("\n"),
+    );
   }
+  const gtr = new GTR();
+  return new TransformStream<string[]>({
+    transform: async (chunk: string[], controller) => {
+      const { trans } = await gtr.translate(chunk.join("\n"), {
+        sourceLang: option.source,
+        targetLang: option.target,
+      });
 
-  async run(_denops: Denops, source: string[]) {
-    const { trans } = await this.gtr.translate(source.join("\n"), {
-      sourceLang: this.option.source,
-      targetLang: this.option.target,
-    });
-
-    return trans.split("\n");
-  }
-}
-
-type Option = {
-  source: string;
-  target: string;
+      controller.enqueue(trans.split("\n"));
+    },
+  });
 };
 
-function isOption(x): x is Option {
-  return isObject(x, isString) && "source" in x && "target" in x;
-}
+export default processor;
